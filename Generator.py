83bd133905d38ec271c6d4152df8caf2c27536d6
zaktaw <zaktaw@gmail.com>
2020-04-29 09:22:10 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -2,11 +2,7 @@ from tkinter.filedialog import askopenfilename
 import random
 
 exercises = []
-
-##tkinter window should close
-file_name = askopenfilename()
-file_content = open(file_name,"r")
-##File shold close after it is read
+userInput = ''
 
 def addExercise(inputExercise):
     inputExercise = inputExercise.split(';')
@@ -19,12 +15,21 @@ def addExercise(inputExercise):
     for x in range(0,exercise_sets):
         exercises.append(exercise)
 
-for exe in file_content:
-    addExercise(exe)
+
 
 def startWorkout():
-  random.shuffle(exercises)
-  for exercise in exercises:
-      input(exercise[0] + ': ' + str(random.randint(exercise[1],exercise[2])))
-      
-startWorkout()
+    ##tkinter window should close
+    file_name = askopenfilename()
+    file_content = open(file_name,"r")
+    for exe in file_content:
+        addExercise(exe)
+    ##File shold close after it is read
+    random.shuffle(exercises)
+    for exercise in exercises:
+        input(exercise[0] + ': ' + str(random.randint(exercise[1],exercise[2])))
+    
+
+
+while userInput != 'esc':
+    startWorkout()  
+    userInput = input('Workout finished. Good job! Type "esc" to exit')
