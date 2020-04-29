9926cfae383f6fa92bda424d6e9d6110b9f381ce
zaktaw <zaktaw@gmail.com>
2020-04-29 07:50:58 +0200

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,7 +1,9 @@
 from tkinter.filedialog import askopenfilename
 
+##tkinter window should close
 file_name = askopenfilename()
 file_content = open(file_name,"r")
 
 for line in file_content:
     print(line)
+##File shold close after it is read
