2acf63084e4760ce663a213a1419c0668989254f
zaktaw <zaktaw@gmail.com>
2020-04-29 08:07:24 +0200

    Made addExercise method

--- Generator.py
+++ Generator.py
@@ -1,9 +1,25 @@
 from tkinter.filedialog import askopenfilename
+import random
+
+exercises = []
 
 ##tkinter window should close
 file_name = askopenfilename()
 file_content = open(file_name,"r")
-
-for line in file_content:
-    print(line)
 ##File shold close after it is read
+
+def addExercise(inputExercise):
+    inputExercise = inputExercise.split(';')
+    exercise_name = inputExercise[0]
+    exercise_min_reps = int(inputExercise[2])
+    exercise_max_reps = int(inputExercise[3])
+    exercise = [exercise_name,exercise_min_reps,exercise_max_reps]
+    exercise_sets = int(inputExercise[1])
+    
+    for x in range(0,exercise_sets):
+        exercises.append(exercise)
+
+for exe in file_content:
+    addExercise(exe)
+
+print(exercises)
